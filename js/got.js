384a1add656abfc4bc50fa9bace96f718a96d02c
Greta Sipos <sipos.greta0330@gmail.com>
2018-07-23 13:34:15 +0200

    prepare characters for display

--- js/got.js
+++ js/got.js
@@ -1,23 +1,25 @@
 function getData(url, callbackFunc) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            callbackFunc(this);
-        }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      callbackFunc(this);
+    }
+  };
+  xhttp.open('GET', url, true);
+  xhttp.send();
 }
 
 function successAjax(xhttp) {
-    // itt a json content, benne a data változóban
-    var userDatas = JSON.parse(xhttp.responseText);
-    console.log(userDatas);
-    /*
+  // itt a json content, benne a data változóban
+  var userDatas = JSON.parse(xhttp.responseText)[2].data;
+  console.log(userDatas);
+  sortCharactersByName(userDatas);
+  removeDeadCharacters(userDatas);
+  /*
       Pár sorral lejebb majd ezt olvashatod:
       IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ!
 
-      Na azokat a függvényeket ITT HÍVD MEG! 
+      Na azokat a függvényeket ITT HÍVD MEG!
 
       A userDatas NEM GLOBÁLIS változó, ne is tegyétek ki globálisra. Azaz TILOS!
       Ha valemelyik függvényeteknek kell, akkor paraméterként adjátok át.
@@ -25,7 +27,32 @@ function successAjax(xhttp) {
 }
 
 // Írd be a json fileod nevét/útvonalát úgy, ahogy nálad van
-getData('/json/aJsonFileodNeve.json', successAjax);
+getData('/json/got-characters.json', successAjax);
 
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+
+function sortCharactersByName(names) {
+  names.sort(function (a, b) {
+    var aName = a.name.toLowerCase();
+    var bName = b.name.toLowerCase();
+    if (aName < bName) {
+      return -1;
+    }
+    if (bName < aName) {
+      return 1;
+    }
+    return 0;
+  });
+  return names;
+}
+
+function removeDeadCharacters(live) {
+  for (var i = 0; i < live.length; i++) {
+    if (parseInt(live[i].dead) == 1) {
+      live.splice(i, 1);
+      i--;
+    }
+  }
+  return live;
+}
